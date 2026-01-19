4afbcb828ab1f6f895c50f9b2ac7969de58d391a
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2026-01-20 01:49:21 +0545

    carosule add

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,3 +1,4 @@
+
 import { useGetBooksQuery } from "../books/bookApi";
 import BookCard from "../books/BookCard";
 
@@ -7,10 +8,17 @@ export default function Home() {
     if(error) return <h1 className = "text-pink-950" >{error}</h1>
   
     return (
+
+        <div>
+            <div>
+               
+            </div>
+            
         <div className="grid grid-cols-4 gap-5 mt-4 items-start" >
             {data.books.map((book) => (
                 <BookCard key={book._id} book={book} />
             ))}
+        </div>
         </div>
     )
 }
