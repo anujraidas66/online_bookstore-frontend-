6bd6abfb9698546d18dc4d1d0af7350d09a3525e
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2026-01-21 12:00:18 +0545

    chagne

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,24 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+
+    // Remove warning — optional (set your own limit)
+    chunkSizeWarningLimit: 1200,
+
+    rollupOptions: {
+      output: {
+        // Auto split big node_modules into separate chunks
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+          }
+        },
+      },
+    },
+  }
 })
