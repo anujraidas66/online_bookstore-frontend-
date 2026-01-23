3c72a2d2009c95992cfd7a8ddc5ea8c25a7ded8a
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2026-01-23 18:04:42 +0545

    change

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,11 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const base = 'https://book-backend-jx5k.onrender.com';
+// export const base = 'https://book-backend-jx5k.onrender.com';
+
+export const base = 'http://192.168.1.75:5000';
+
 
 export const mainApi = createApi({
     reducerPath: 'mainApi',
     baseQuery: fetchBaseQuery({
-         baseUrl: 'https://book-backend-jx5k.onrender.com/api',
+        //  baseUrl: 'https://book-backend-jx5k.onrender.com/api',
+        baseUrl: 'http://192.168.1.75:5000/api',
     }),
     endpoints: (builder) => ({}),
 });
